dadc8b115cd32193c6334d78e0890c334af4230a
Jeferson Oliveira <zuruckyt@gmail.com>
2021-05-15 17:39:41 -0300

    Add rota para pagina da raça

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,24 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import Home from '../views/Home.vue';
+import BreedDetail from '../views/BreedDetail.vue';
 
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
   },
-]
+  {
+    path: '/breed/:breed_name',
+    name: 'breed',
+    props: true,
+    component: BreedDetail
+  }
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
-})
+});
 
 export default router
